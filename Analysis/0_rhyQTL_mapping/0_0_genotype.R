ec45201e37733593131f7efdb18cbdd2dfe6ad86
YingChen10 <44314773+YingChen10@users.noreply.github.com>
2025-01-10 14:35:09 -0600

    Retrieval of gene - genetic locus pairs

--- Analysis/0_rhyQTL_mapping/0_0_genotype.R
+++ Analysis/0_rhyQTL_mapping/0_0_genotype.R
@@ -7,21 +7,27 @@ suppressMessages(library("dryR"))
 suppressMessages(library("lmtest"))
 
 ################## parameters ###########################
-dir <- "Projects/rQTL/"
+dir <- "/N/scratch/cc123/gwas/rQTL/"
 cat <- "cis_QTL/"
 
 args <- commandArgs(trailingOnly = TRUE)
 tissue <- args[1]
 pos_file <- args[2]
 
+# tissue <- 'Brain-Substantianigra'
+# pos_file <- 'aa'
+outdir <- paste0(dir, cat, '00_rQTL_mapping/00_Genotype/', tissue, '/')
+logoutdir <- paste0(dir, cat, 'Log/00_Genotype/', tissue, '/')
+if (!file.exists(outdir)) {dir.create(outdir, recursive = TRUE)}
+if (!file.exists(logoutdir)) {dir.create(logoutdir, recursive = TRUE)}
 
-if (!file.exists(paste0(dir, cat, '00_rQTL_mapping_00_Genotype/', tissue))) {
-  dir.create(paste0(dir, cat, '00_rQTL_mapping_00_Genotype/', tissue))}
 
+a <- Sys.time()
+write.table(a, paste0(logoutdir, pos_file), quote = F, row.names = F, col.names = F)
 
 min_size <- 50
 period <- 24
-group  <- 2 # keep snp with sample size greater than min_size in at least 2 genotype groups
+group  <- 2 # keep snv with sample size gt  min_size in at least this group number
 #######################################################
 
 #################### functions ########################
@@ -35,11 +41,11 @@ Filter_SNV <- function(j){
   geno$genotype <- as.numeric(substring(geno$V1,1,1)) + as.numeric(substring(geno$V1,3,3))
   num <- as.data.frame(table(geno$genotype))
   
-  # only keep genotype groups with sample size >= min_size 
+  # only keep wt group with > min size 
   num_cp <- filter(num, Freq >= min_size)
   
   if(nrow(num_cp) >= 2){
-    # subset samples with their genotype
+    # subset geno
     geno <- filter(geno, genotype %in% as.character(num$Var1))
   
     # return 
@@ -50,10 +56,10 @@ Filter_SNV <- function(j){
 
 
 ###### import files ##############
-### time information
-time <- fread(paste0(dir, 'GTEx_donor_time.txt'))
+### input time information
+time <- fread(paste0(dir, 'GTEx_donor_time_science.txt'))
 
-### gene position file, TSS information of 18,200 protein coding genes are split into 49 files in order to reduce the calculation time
+###### input gene position
 pos <- read.table(paste0(dir, cat, "split_pos/", pos_file))
 pos$tss <- pos$V2
 pos$tss[pos$V4 == '-'] <- pos$V3[pos$V4 == '-']
@@ -64,60 +70,55 @@ pos$tss_up[pos$tss_up < 0] <- 0
 ### head of snv file
 head <- read.table(paste0(dir, 'head_sub.txt'))
 
-### input expression file
-expression <- fread(paste0(dir,'GTEx_nor_expression/',tissue, '.txt'))
-expression <- as.data.frame(expression)
-expression <- expression[,1:(ncol(expression) - 34)]
-expression$EnsemblID <- sapply(strsplit(expression$EnsemblID, '_'), "[", 1)
-names(expression)[2:ncol(expression)] <- sapply(strsplit(names(expression)[2:ncol(expression)], '\\.'), "[", 2)
+# import expression file
+expression <- read.table(paste0(dir,'00_data/CPM_covariate_remove/', tissue, '.txt'), header = T)
+names(expression) <- str_split_fixed(names(expression), '\\.', 2)[ ,2]
+expression$EnsemblID <- str_split_fixed(row.names(expression), '_', 2)[ ,1]
 
-########## retrieve genotype information at each cis-variant locus for each gene #########################
+
+########## retrieve genotype information ##############################
 all_results <- list()
 for(line in  1:nrow(pos)){
   print (paste0('Starting processing gene', line))
-  
-  # get TSS-1M flanking region for the gene
+  ## retrieve TSS-1M flanking region  for each gene
   gene_info <- unlist(pos[line, c(1,12,13)])
   region <- paste0(gene_info[1], ':', gene_info[2], '-', gene_info[3])
   gene <- pos$V5[line]
   
 
-  # retrieve SNVs within TSS-1M flanking region for the gene
-  output <- system(paste0("tabix ", dir, "GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.MAF01.hwe_MAF059.vcf.gz ", region), intern=TRUE)
+  # retrieve SNVs in TSS-1M flanking region  for each gene
+  output <- system(paste0("/geode2/home/u020/cc123/BigRed200/miniconda3/bin/tabix ", dir, "GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.MAF01.hwe_MAF059.vcf.gz ", region), intern=TRUE)
+  # output <- system(paste0("/workspace/rsrch1/ychen/miniconda3/bin/tabix ", dir, "GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.MAF01.hwe_MAF059.vcf.gz ", region), intern=TRUE)
  
   snv <- read.table(textConnection(output), header=F, sep="\t")
   names(snv) <- as.character(head)
   snv <- snv[grepl("A|G|C|T", snv$REF, perl = T), ]
   snv <- snv[grepl("A|G|C|T", snv$ALT, perl = T), ]
   if(nrow(snv) == 0){
-    
-    print ('no cis SNVs')
+    #   print ('no cis SNVs')
     #all_results[[line]] <- NULL
     next}
+  #print('1. Retrieving SNVs in cis region done!')
   
-  print('1. Retrieving SNVs in cis region done!')
   
-  
-  # retrieve gene expression matrix for the gene
+  ## retrieve gene expression matrix for the gene
   expr <- filter(expression, EnsemblID == gene) 
-  if(nrow(expr) == 0){ 
-    print ('no gene in expression file')
+  if(nrow(expr) == 0){ #   print ('no gene in expr file')
     #all_results[[line]] <- NULL
     next}
-  
-  # remove samples in which the gene expression was not detected
+  # remove sample with expression of the gene didn't detected
   expr <- select(expr, names(expr)[!is.na(expr)])
-  print('2. Retrieving expression matrix done!')
+  # print('2. Retrieving expression matrix done!')
   
   
-  ## filter snv according to the sample id in expression matrix
+  ## filter snv according samples in expression matrix
   snv_interscet <- select(snv, ID, intersect(names(expr)[-1], names(snv)))
   
   geno_list <- lapply(1:nrow(snv_interscet), Filter_SNV)
   names(geno_list) <- snv_interscet$ID
   geno_list <- geno_list[!sapply(geno_list, is.null)]
   if(length(names(geno_list)) == 0){
-    print ('After filtering, there are no cis SNVs remaining')
+    #   print ('no cis SNVs after filter')
     #all_results[[line]] <- NULL
     next}
   
@@ -125,6 +126,7 @@ for(line in  1:nrow(pos)){
 }
 
 all_results <-  all_results[!sapply(all_results, is.null)]
+saveRDS(all_results, paste0(outdir, pos_file, '.rds'))
 
-saveRDS(all_results, paste0(dir, cat, '00_rQTL_mapping_00_Genotype/', tissue, '/', pos_file, '.rds'))
-print (Sys.time())
+b <- data.frame(tissue = tissue, file = pos_file, s = a, e = Sys.time())
+write.table(b, paste0(logoutdir, pos_file, '.complete'), sep = '\t', quote = F, row.names = F, col.names = F)
